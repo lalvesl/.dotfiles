2baaf9e82921be68873849297365b39e812ee47c
Lucas Alves de Lima <lucaslima@weg.net>
2024-08-02 01:18:01 +0000

    feat: add rust-tools

--- .config/nvim/lua/plugins/rust-tool.lua
+++ .config/nvim/lua/plugins/rust-tool.lua
@@ -1,3 +1,10 @@
 return {
-	"simrat39/rust-tools.nvim",
+	"mrcjkb/rustaceanvim",
+	version = "^5", -- Recommended
+	lazy = false, -- This plugin is already lazy
 }
+
+--
+-- return {
+-- 	"simrat39/rust-tools.nvim",
+-- }
