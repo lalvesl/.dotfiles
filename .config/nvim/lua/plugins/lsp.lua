8e626059df39abdf422273323a67660bfc1d3b65
Lucas Alves de Lima <lucaslima@weg.net>
2024-08-02 01:21:27 +0000

    feat: implement tailwind integration with rust files

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -25,8 +25,44 @@ return {
 			servers = {
 				cssls = {},
 				tailwindcss = {
+					filetypes = {
+						"css",
+						"scss",
+						"sass",
+						"postcss",
+						"html",
+						"javascript",
+						"javascriptreact",
+						"typescript",
+						"typescriptreact",
+						"svelte",
+						"vue",
+						"rust",
+						"rs",
+					},
+					experimental = {
+						classRegex = {
+							[[class="([^"]*)]],
+							'class=\\s+"([^"]*)',
+						},
+					},
+					init_options = {
+						userLanguages = {
+							eelixir = "html-eex",
+							eruby = "erb",
+							rust = "html",
+						},
+					},
 					root_dir = function(...)
-						return require("lspconfig.util").root_pattern(".git")(...)
+						-- return require("lspconfig.util").root_pattern(".git")(...)
+						return require("lspconfig.util").root_pattern(
+							"tailwind.config.js",
+							"tailwind.config.ts",
+							"postcss.config.js",
+							"postcss.config.ts",
+							"package.json",
+							"node_modules"
+						)(...)
 					end,
 				},
 				tsserver = {
